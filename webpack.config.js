5ac75be7701326a895de3d9fa99b49ae50cbf9b4
Raj Kalyan Kumar <rajkalyan.dev@gmail.com>
2024-12-16 22:59:43 +0530

    added configurations

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+console.clear();
+console.log("Bundeling....")
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js',
+    },
+    mode: "development",
+    devServer: {
+        static: {
+            directory: path.join(__dirname, 'public'),
+        },
+        compress: true,
+        port: 5000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        targets: "defaults",
+                        presets:
+                            ['@babel/preset-env', "@babel/preset-react"]
+                    }
+                }
+            }
+        ],
+
+    },
+};
